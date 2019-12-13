ddf7e66610962bed658d36ce01e7f7d0cc977bc2
Heckfy <Heckfy@HECKFYPC>
2019-12-13 06:06:04 +0300

    начал делать новую форму для отображения абонента

---
+++ WPF_MVVM_EF_Demo/Convertors/HeaderToImageConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using Demo.UI.Views;
+
+namespace Demo.UI.Convertors
+{
+    /// <summary>
+    /// Переделать под MVVM
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Получить полный путь
+            var path = (string)value;
+
+            // Если путь пустой игнорировать
+            if (path == null)
+                return null;
+
+            // Получить имя файла/папки
+            var name = AbonentView.GetFileFolderName(path);
+
+            // By default, we presume an image
+            var image = "Images/file.png";
+
+            // If the name is blank, we presume it's a drive as we cannot have a blank file or folder name
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/folder-closed.png";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
